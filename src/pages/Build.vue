34f8fd034f3af20f6a8d243d5dcd65ad66c294ba
Ricardo Boss <contact@ricardoboss.de>
2021-03-07 03:01:12 +0100

    Refactored Index to Build

---
+++ src/pages/Build.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <div class="row q-mt-lg">
+      <h5 class="q-my-sm">New Build 1</h5>
+      <span class="text-grey-6">created 1 day ago</span>
+      </div>
+
+    <q-separator></q-separator>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: 'build',
+        component: () => import('pages/Build.vue')
+      }
     ]
   },
 
