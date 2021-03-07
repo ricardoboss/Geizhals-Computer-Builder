0cee896a66bdecb887291f1f8fc3f1bc51a38744
Ricardo Boss <contact@ricardoboss.de>
2021-03-07 17:23:18 +0100

    Added data object for holding component information

--- src/pages/Build.vue
+++ src/pages/Build.vue
@@ -1,17 +1,34 @@
 <template>
   <q-page>
-    <div class="row q-mt-lg">
+    <div class="flex column">
       <h5 class="q-my-sm">New Build 1</h5>
       <span class="text-grey-6">created 1 day ago</span>
+    </div>
+
+    <q-separator class="q-my-lg" />
+
+    <div class="flex row">
+      <div class="flex column">
+
       </div>
-
-    <q-separator></q-separator>
-
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Build'
+  name: 'Build',
+
+  data() {
+    return {
+      parts: {
+        case: null,
+        motherboard: null,
+        cpu: null,
+        ram: null,
+        gpu: null
+      }
+    }
+  },
 }
 </script>
