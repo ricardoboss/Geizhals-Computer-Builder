a0d7c2c86005046bffa8f698edf9b5b22e7c3dcf
Ricardo Boss <contact@ricardoboss.de>
2021-03-07 03:00:54 +0100

    Removed drawer and updated title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,39 +4,22 @@
     <q-header class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
+          geizhals Computer Builder
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="right = !right"/>
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One"/>
-        <q-route-tab to="/page2" label="Page Two"/>
-        <q-route-tab to="/page3" label="Page Three"/>
+        <q-route-tab to="/build" label="Build"/>
       </q-tabs>
     </q-header>
-
-    <q-drawer show-if-above v-model="right" side="right" elevated>
-      <!-- drawer content -->
-    </q-drawer>
 
     <q-page-container>
       <router-view/>
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      right: false
-    }
-  }
 }
 </script>
